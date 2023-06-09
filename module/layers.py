d7828080e16fd188d5eb9373b09ea52b89c8d805
mdaniyalk <muchammad.daniyal.kautsar@mail.ugm.ac.id>
2023-06-09 09:09:45 +0700

    fix: lstm layer

--- module/layers.py
+++ module/layers.py
@@ -210,107 +210,259 @@ class Flatten():
 
 
 # LSTM
-class LSTM(Dense):
+
+class LSTM:
     """
-    Long Short-Term Memory (LSTM) layer implementation.
+    Long Short-Term Memory (LSTM) neural network implementation.
 
-    This class inherits from the `Dense` class and extends it to implement the LSTM layer. The LSTM layer is a type of recurrent neural network (RNN) layer that is capable of capturing long-term dependencies in sequential data.
+    Args:
+    - input_shape (tuple): The shape of the input data (batch size, sequence length, input features).
+    - num_hidden_layers (int): The number of hidden layers in the LSTM.
+    - weight_regularizer_l1 (float): L1 regularization strength for the weight parameters.
+    - weight_regularizer_l2 (float): L2 regularization strength for the weight parameters.
+    - bias_regularizer_l1 (float): L1 regularization strength for the bias parameters.
+    - bias_regularizer_l2 (float): L2 regularization strength for the bias parameters.
 
-    Parameters:
-    - input_size: The size of the input to the LSTM layer.
-    - hidden_size: The number of hidden units in the LSTM layer.
+    This class implements the Long Short-Term Memory (LSTM) architecture, a type of recurrent neural network (RNN). LSTMs are capable of learning long-term dependencies and are commonly used for sequence modeling tasks.
 
-    The LSTM layer is initialized by calling the constructor of the `Dense` class with modified input and output sizes. The `hidden_size` parameter determines the number of hidden units in the LSTM layer.
+    The class initializes the LSTM with the specified `input_shape` and `num_hidden_layers`. It also allows optional regularization for the weight and bias parameters.
 
-    The forward method performs the forward pass of the LSTM layer. It takes the inputs and previous hidden state as arguments and computes the new hidden state and output. The method applies the sigmoid and tanh activations to different parts of the concatenated input and updates the cell state and output based on these activations.
+    The LSTM consists of forward and backward methods for performing forward and backward propagation, respectively. It maintains the LSTM's internal states, including the hidden state and cell state.
 
-    The backward method performs the backward pass of the LSTM layer. It takes the gradients of the output and cell state with respect to the loss and computes the gradients of the input gate, forget gate, output gate, and cell state update. The method then backpropagates these gradients through the `Dense` class to update the weights and biases.
+    The forward method takes the input data and performs forward propagation through the LSTM, updating the internal states and producing an output. The backward method performs backward propagation to calculate the gradients of the parameters.
 
-    The get_parameters method returns the weights and biases of the LSTM layer.
+    The LSTM provides methods to get and set the parameters of the network.
 
     Example usage:
-    lstm = LSTM(input_size=10, hidden_size=32)
-    lstm.forward(inputs, state)
-    lstm.backward(dh, dc)
-    weights, biases = lstm.get_parameters()
+    lstm = LSTM(input_shape=(batch_size, sequence_length, input_features), num_hidden_layers=2)
+    lstm.forward(inputs, training=True)
+    lstm.backward(dvalues)
+    parameters = lstm.get_parameters()
+    lstm.set_parameters(parameters)
     """
-
-    def __init__(self, input_size, hidden_size):
+     
+    def __init__(self, input_shape, num_hidden_layers, weight_regularizer_l1=0, weight_regularizer_l2=0,
+                 bias_regularizer_l1=0, bias_regularizer_l2=0):
         """
-        Initialize the LSTM layer.
+        Initialize the LSTM network.
 
         Args:
-        - input_size: The size of the input to the LSTM layer.
-        - hidden_size: The number of hidden units in the LSTM layer.
+        - input_shape (tuple): The shape of the input data.
+        - num_hidden_layers (int): The number of hidden layers in the LSTM network.
+        - weight_regularizer_l1 (float): L1 regularization strength for weights (default: 0).
+        - weight_regularizer_l2 (float): L2 regularization strength for weights (default: 0).
+        - bias_regularizer_l1 (float): L1 regularization strength for biases (default: 0).
+        - bias_regularizer_l2 (float): L2 regularization strength for biases (default: 0).
 
-        The LSTM layer is initialized by calling the constructor of the `Dense` class with modified input and output sizes. The `hidden_size` parameter determines the number of hidden units in the LSTM layer.
-        """
-        super().__init__(input_size + hidden_size, 4 * hidden_size)
-        self.hidden_size = hidden_size
+        This method initializes the LSTM network by setting various attributes and initializing the weights and biases.
 
-    def forward(self, inputs, state):
+        The `input_shape` parameter specifies the shape of the input data. The `num_hidden_layers` parameter determines the number of hidden layers in the LSTM network.
+
+        The `weight_regularizer_l1`, `weight_regularizer_l2`, `bias_regularizer_l1`, and `bias_regularizer_l2` parameters allow specifying L1 and L2 regularization strengths for the weights and biases. These regularization terms help prevent overfitting by adding a penalty to the loss function based on the magnitudes of the weights and biases.
+
+        The weights and biases are randomly initialized using a normal distribution with a mean of 0 and a standard deviation of 0.01. The dimensions of the weights and biases are determined based on the input shape and the number of hidden layers.
+
+        The attributes `hidden_state`, `cell_state`, `prev_hidden_state`, and `prev_cell_state` are initialized to `None` or arrays of zeros.
+
+        Example usage:
+        lstm = LSTM(input_shape=(10, 5), num_hidden_layers=2, weight_regularizer_l2=0.001)
         """
-        Perform the forward pass of the LSTM layer.
+
+        self.num_features = input_shape[1]
+        self.num_hidden_layers = num_hidden_layers
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
+        
+        # Initialize weights and biases
+        self.wf = 0.01 * np.random.randn(self.num_features + num_hidden_layers, num_hidden_layers)
+        self.wi = 0.01 * np.random.randn(self.num_features + num_hidden_layers, num_hidden_layers)
+        self.wc = 0.01 * np.random.randn(self.num_features + num_hidden_layers, num_hidden_layers)
+        self.wo = 0.01 * np.random.randn(self.num_features + num_hidden_layers, num_hidden_layers)
+        
+        self.bf = np.zeros((1, num_hidden_layers))
+        self.bi = np.zeros((1, num_hidden_layers))
+        self.bc = np.zeros((1, num_hidden_layers))
+        self.bo = np.zeros((1, num_hidden_layers))
+        
+        self.hidden_state = None
+        self.cell_state = None
+        self.prev_hidden_state = None
+        self.prev_cell_state = None
+    
+    def forward(self, inputs, training):
+        """
+        Perform the forward pass of the LSTM network.
 
         Args:
-        - inputs: The input to the LSTM layer.
-        - state: The previous hidden state of the LSTM layer.
+        - inputs (ndarray): The input data.
+        - training (bool): Whether the network is in training mode.
 
-        Returns:
-        - output: The output of the LSTM layer.
-        - new_state: The new hidden state of the LSTM layer.
-
-        The forward method performs the forward pass of the LSTM layer. It takes the inputs and previous hidden state as arguments and computes the new hidden state and output. The method applies the sigmoid and tanh activations to different parts of the concatenated input and updates the cell state and output based on these activations.
+        This method calculates the hidden and cell states for each step in the input data.
+        The resulting hidden state is stored in the `output` attribute.
         """
-        concatenated = np.concatenate([inputs, state], axis=1)
-        super().forward(concatenated, training=True)
-        self.ig, self.fg, self.og, self.cu = np.split(self.output, 4, axis=1)
-        self.ig = 1 / (1 + np.exp(-self.ig))
-        self.fg = 1 / (1 + np.exp(-self.fg))
-        self.og = 1 / (1 + np.exp(-self.og))
-        self.cu = np.tanh(self.cu)
-        self.c = self.fg * state + self.ig * self.cu
-        self.h = self.og * np.tanh(self.c)
-        return self.h, self.c
 
-    def backward(self, dh, dc):
+        batch_size, steps, _ = inputs.shape
+        self.hidden_state = np.zeros((batch_size, self.num_hidden_layers))
+        self.cell_state = np.zeros((batch_size, self.num_hidden_layers))
+        self.prev_hidden_state = np.zeros_like(self.hidden_state)
+        self.prev_cell_state = np.zeros_like(self.cell_state)
+        
+        self.inputs = inputs
+        self.cell_states = np.zeros((batch_size, steps, self.num_hidden_layers))
+        self.hidden_states = np.zeros((batch_size, steps, self.num_hidden_layers))
+        
+        for step in range(steps):
+            self.prev_hidden_state = self.hidden_state
+            self.prev_cell_state = self.cell_state
+            
+            inputs_step = np.expand_dims(inputs[:, step, :], axis=1)
+
+            concatenated_input = np.concatenate((inputs[:, step, :], self.hidden_state), axis=1)
+            
+            ft = sigmoid(np.dot(concatenated_input, self.wf) + self.bf)
+            it = sigmoid(np.dot(concatenated_input, self.wi) + self.bi)
+            ct = np.tanh(np.dot(concatenated_input, self.wc) + self.bc)
+            ot = sigmoid(np.dot(concatenated_input, self.wo) + self.bo)
+            
+            self.cell_state = ft * self.prev_cell_state + it * ct
+            self.hidden_state = ot * np.tanh(self.cell_state)
+            
+            self.cell_states[:, step, :] = self.cell_state
+            self.hidden_states[:, step, :] = self.hidden_state
+        
+        self.output = self.hidden_state
+        
+    def backward(self, dvalues):
         """
-        Perform the backward pass of the LSTM layer.
+        Perform the backward pass of the LSTM network.
 
         Args:
-        - dh: The gradients of the output with respect to the loss.
-        - dc: The gradients of the cell state with respect to the loss.
+        - dvalues (ndarray): The gradient of the loss with respect to the output.
 
-        Returns:
-        - dinputs: The gradients with respect to the input to the LSTM layer.
-        - dstate: The gradients with respect to the previous hidden state of the LSTM layer.
-
-        The backward method performs the backward pass of the LSTM layer. It takes the gradients of the output and cell state with respect to the loss and computes the gradients of the input gate, forget gate, output gate, and cell state update. The method then backpropagates these gradients through the `Dense` class to update the weights and biases.
+        This method calculates the gradients for the weights, biases, and input data.
+        Regularization is applied to the weight and bias gradients.
         """
-        dho = dh * np.tanh(self.c)
-        dc = dc + dh * self.og * (1 - np.tanh(self.c) ** 2)
-        dcu = dc * self.ig * (1 - self.cu ** 2)
-        dig = dc * self.cu * self.ig * (1 - self.ig)
-        dfg = dc * self.fg * (1 - self.fg)
-        dog = dho * np.tanh(self.c) * self.og * (1 - self.og)
-        gradients = np.concatenate([dig, dfg, dog, dcu], axis=1)
-        super().backward(gradients)
-        dconcat = self.dinputs
-        dstate = dconcat[:, self.input_size:]
-        dinputs = dconcat[:, :self.input_size]
-        return dinputs, dstate
 
+        batch_size, steps, _ = self.inputs.shape
+        self.dinputs = np.zeros_like(self.inputs)
+        self.dwf = np.zeros_like(self.wf)
+        self.dwi = np.zeros_like(self.wi)
+        self.dwc = np.zeros_like(self.wc)
+        self.dwo = np.zeros_like(self.wo)
+        self.dbf = np.zeros_like(self.bf)
+        self.dbi = np.zeros_like(self.bi)
+        self.dbc = np.zeros_like(self.bc)
+        self.dbo = np.zeros_like(self.bo)
+        
+        dh_next = np.zeros_like(self.hidden_state)
+        dc_next = np.zeros_like(self.cell_state)
+        
+        for step in reversed(range(steps)):
+            dho = dvalues
+            dc = dh_next * sigmoid_derivative(np.tanh(self.cell_states[:, step, :])) + dc_next
+            do = dho * np.tanh(self.cell_states[:, step, :])
+            di = dc * np.tanh(self.cell_states[:, step, :])
+            df = dc * self.prev_cell_state
+            dc_prev = dc * self.prev_cell_state
+            dg = di * sigmoid_derivative(np.dot(np.concatenate((self.inputs[:, step, :], self.hidden_state), axis=1), self.wc) + self.bc)
+
+            
+            self.dwf += np.dot(np.concatenate((self.inputs[:, step, :], self.hidden_states[:, step, :]), axis=1).T, df)
+            self.dwi += np.dot(np.concatenate((self.inputs[:, step, :], self.hidden_states[:, step, :]), axis=1).T, di)
+            self.dwc += np.dot(np.concatenate((self.inputs[:, step, :], self.hidden_states[:, step, :]), axis=1).T, dg)
+            self.dwo += np.dot(np.concatenate((self.inputs[:, step, :], self.hidden_states[:, step, :]), axis=1).T, do)
+            self.dbf += np.sum(df, axis=0, keepdims=True)
+            self.dbi += np.sum(di, axis=0, keepdims=True)
+            self.dbc += np.sum(dg, axis=0, keepdims=True)
+            self.dbo += np.sum(do, axis=0, keepdims=True)
+            
+            self.dinputs[:, step, :] = np.dot(dc_prev, self.wc.T)[:, :self.num_features]
+            dh_next = np.dot(dc_prev, self.wc.T)[:, self.num_features:]
+            dc_next = dc * self.prev_cell_state
+        
+        # Regularization
+        self.dwf += self.weight_regularizer_l1 * np.sign(self.wf) + \
+                    self.weight_regularizer_l2 * 2 * self.wf
+        self.dwi += self.weight_regularizer_l1 * np.sign(self.wi) + \
+                    self.weight_regularizer_l2 * 2 * self.wi
+        self.dwc += self.weight_regularizer_l1 * np.sign(self.wc) + \
+                    self.weight_regularizer_l2 * 2 * self.wc
+        self.dwo += self.weight_regularizer_l1 * np.sign(self.wo) + \
+                    self.weight_regularizer_l2 * 2 * self.wo
+        self.dbf += self.bias_regularizer_l1 * np.sign(self.bf) + \
+                    self.bias_regularizer_l2 * 2 * self.bf
+        self.dbi += self.bias_regularizer_l1 * np.sign(self.bi) + \
+                    self.bias_regularizer_l2 * 2 * self.bi
+        self.dbc += self.bias_regularizer_l1 * np.sign(self.bc) + \
+                    self.bias_regularizer_l2 * 2 * self.bc
+        self.dbo += self.bias_regularizer_l1 * np.sign(self.bo) + \
+                    self.bias_regularizer_l2 * 2 * self.bo
+        
     def get_parameters(self):
         """
-        Get the parameters of the LSTM layer.
+        Get the parameters (weights and biases) of the LSTM network.
 
         Returns:
-        - weights: The weights of the LSTM layer.
-        - biases: The biases of the LSTM layer.
+        - parameters (list): A list containing the parameters.
 
-        The get_parameters method returns the weights and biases of the LSTM layer.
+        This method returns a list of all the parameters (weights and biases) of the LSTM network.
         """
-        return self.weights, self.biases
+
+        return [self.wf, self.wi, self.wc, self.wo, self.bf, self.bi, self.bc, self.bo]
+    
+    def set_parameters(self, parameters):
+        """
+        Set the parameters (weights and biases) of the LSTM network.
+
+        Args:
+        - parameters (list): A list containing the new parameters.
+
+        This method allows setting the parameters of the LSTM network using a provided list.
+        """
+
+        self.wf, self.wi, self.wc, self.wo, self.bf, self.bi, self.bc, self.bo = parameters
+        
+def sigmoid(x):
+    """
+    Compute the sigmoid activation function.
+
+    Args:
+    - x (ndarray): The input array.
+
+    Returns:
+    ndarray: The computed sigmoid values.
+
+    The sigmoid function is a non-linear activation function that squashes input values to a range between 0 and 1.
+
+    This function computes the sigmoid activation element-wise for each element in the input array.
+
+    Example usage:
+    result = sigmoid(np.array([-1, 0, 1]))
+    """
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    """
+    Compute the derivative of the sigmoid activation function.
+
+    Args:
+    - x (ndarray): The input array.
+
+    Returns:
+    ndarray: The computed sigmoid derivative values.
+
+    The derivative of the sigmoid function is used to compute the gradients during backpropagation.
+
+    This function computes the derivative of the sigmoid activation element-wise for each element in the input array.
+
+    Example usage:
+    result = sigmoid_derivative(np.array([-1, 0, 1]))
+    """
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
 
 
 
